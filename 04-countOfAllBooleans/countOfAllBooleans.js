982da21c40e415be586cafa2f36f43e5494378fe
CyberAlv <munaralvin@gmail.com>
2020-05-31 16:21:44 -0400

    Added code

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let count = 0;
+   for(x = 0; x < arr.length; x++){
+      if(arr[x] == true || arr[x] == false){
+        count++;
+      }
+   }
+   return count;
 }
 
 // Do not edit this line;
