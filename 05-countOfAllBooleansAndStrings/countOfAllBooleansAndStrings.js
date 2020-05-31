95c24faa42d95327c36cdbb3a49ffff0a724a483
CyberAlv <munaralvin@gmail.com>
2020-05-31 17:41:20 -0400

    Added solution

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let count = 0;
+   for(x = 0; x < arr.length; x++){
+      if(arr[x] == true || arr[x] == false || typeof arr[x]){
+        count++;
+      }
+   }
+   return count;
 }
 
 // Do not edit this line;
