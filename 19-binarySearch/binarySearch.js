cac87e21faa744892de3ad5e565d453a11ae0303
CyberAlv <munaralvin@gmail.com>
2020-05-31 23:51:10 -0400

    Added solution

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,25 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    let start = 0;
+    let middle;
+    let finish = nums.length-1;
+
+    while(start <= finish)
+    {
+      middle =  Math.floor((start + finish) / 2);
+
+      if( nums[middle] == target)
+        return true;
+     else{ 
+        if( target < nums[middle] )
+              finsih = middle -1;
+            else
+                start = middle +1;
+     }
+    }
+    return false;
+
   }
 }
 
